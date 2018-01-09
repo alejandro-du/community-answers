3688055b37259f93b6c3d2e15bd076e3369cfaff
Alejandro Duarte <alejandro@vaadin.com>
2018-01-09 11:59:03 +0200

    In sych with video

--- click-image-in-grid/src/main/java/org/test/VaadinUI.java
+++ click-image-in-grid/src/main/java/org/test/VaadinUI.java
@@ -9,21 +9,18 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.renderers.ImageRenderer;
 
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Theme("mytheme")
 public class VaadinUI extends UI {
 
-    private Grid<Integer> grid = new Grid();
+    private Grid<Integer> grid = new Grid<>();
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        grid.addColumn(i -> i).setCaption("Number"); // adds a "normal" column
-        addIconColumn(); // adds a column with clickable items and images
-
-        // add some rows
-        grid.setItems(IntStream.range(1, 21).boxed().collect(Collectors.toList()));
+        grid.addColumn(i -> i).setCaption("Number");
+        addIconColumn();
+        grid.setItems(IntStream.range(1, 21).boxed());
 
         final VerticalLayout layout = new VerticalLayout(grid);
         setContent(layout);
@@ -31,13 +28,13 @@ public class VaadinUI extends UI {
 
     private void addIconColumn() {
         ImageRenderer<Integer> renderer = new ImageRenderer<>();
-        renderer.addClickListener(e -> iconClicked(e.getItem())); // allow clicks on the image
+        renderer.addClickListener(e -> iconClicked(e.getItem()));
 
-        Grid.Column<Integer, ThemeResource> iconColumn = grid.addColumn(i -> new ThemeResource("img/icon.svg"), renderer)
-                .setCaption("Icon");
+        Grid.Column<Integer, ThemeResource> iconColumn =
+                grid.addColumn(i -> new ThemeResource("img/icon.svg"), renderer);
 
-        grid.addItemClickListener(e -> { // allow clicks on the cell
-            if (iconColumn.equals(e.getColumn())) {
+        grid.addItemClickListener(e -> {
+            if (e.getColumn().equals(iconColumn)) {
                 iconClicked(e.getItem());
             }
         });
